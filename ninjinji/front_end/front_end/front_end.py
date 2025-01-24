a7cc6fa25c7be03c103973a49ece9637d4ecc177
Brandon-lz <advance_to@hotmail.com>
2025-01-24 10:28:40 +0800

    test

--- ninjinji/front_end/front_end/front_end.py
+++ ninjinji/front_end/front_end/front_end.py
@@ -56,3 +56,6 @@ app.add_page(custom_sidebar.index, route="/custom_siderbar")
 from front_end.components.dashboard.page import index as dash
 
 app.add_page(dash, route="/dashboard/dash")
+
+
+# afadf
