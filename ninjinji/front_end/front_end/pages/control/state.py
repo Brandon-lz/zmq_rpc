cba9ef223de8dec3d35d6437cdf9cbc101b390fb
Brandon-lz <advance_to@hotmail.com>
2025-03-01 16:39:14 +0800

    modified:   ninjinji/front_end/front_end/pages/control/state.py

--- ninjinji/front_end/front_end/pages/control/state.py
+++ ninjinji/front_end/front_end/pages/control/state.py
@@ -758,7 +758,7 @@ class StartButtonState(rx.State):
                 torqueChartstate.update_data(chart_new_value)
 
                 # if not self._runing or self.process_count >= self.process_max:
-                if not self._runing or finished_count>=70:
+                if not self._runing or finished_count>=30:
                     self.start_button_text = "完成"
                     self._runing = False
                     return
